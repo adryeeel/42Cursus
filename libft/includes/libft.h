685d5cb217efdeb3f76ab176cb850014b1ffd56c
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-10 00:52:41 +0100

    added ft_isalnum

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/06 00:25:46 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/10 00:19:24 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 typedef unsigned long size_t;
 
-int ft_isdigit(int c);
 int ft_isalpha(int c);
+int ft_isalnum(int c);
+int ft_isdigit(int c);
 
 size_t ft_strlen(const char *s);
 
