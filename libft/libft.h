86ac697aacf0a7f6b2565b6826cffb04ce054c9c
Adryel Rocha Bezerra <arocha-b@c1r4s7.42porto.com>
2023-10-11 11:13:02 +0100

    ft_memchr prototype

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/10 19:06:39 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/11 11:06:53 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 
-void	*ft_memcpy(void *dest, const char *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
+void	*ft_memcpy(void *dest, const char *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
 int		ft_tolower(int c);
@@ -34,5 +35,4 @@ void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
 #endif
