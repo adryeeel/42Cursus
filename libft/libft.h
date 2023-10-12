1d49611c2faf1313d1a2becc73bfd2de62fc939c
Adryel Rocha Bezerra <arocha-b@c1r3s7.42porto.com>
2023-10-12 16:19:46 +0100

    ft_split prototype

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/12 14:14:27 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/12 15:27:00 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int		ft_isalnum(int c);
 int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
@@ -29,15 +31,13 @@ void	*ft_memcpy(void *dest, const char *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(const char *s, char c);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strtrim(const char *s1, const char *set);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
