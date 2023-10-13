8227b46d1fb9b66e96b3c4e8927f0d78e72bc5cf
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-13 21:48:06 +0100

    lstdelone prototype

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/13 18:14:39 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/13 21:42:08 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,6 @@ t_list				*ft_lstlast(t_list *lst);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstadd_front(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
