d9c830923b6f006bdfd503a64e9132a0e27461f3
Adryel Rocha Bezerra <arocha-b@c1r4s8.42porto.com>
2023-10-13 12:03:21 +0100

    ft_itoa prototype

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/12 15:27:00 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:20:57 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	*ft_memcpy(void *dest, const char *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
+char  *ft_itoa(int n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *str);
 char	*ft_strchr(const char *s, int c);
