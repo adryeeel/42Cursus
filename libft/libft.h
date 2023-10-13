4d187172b9f6e1b3b5c5dd6481df204a02533c22
Adryel Rocha Bezerra <arocha-b@c1r4s8.42porto.com>
2023-10-13 12:03:55 +0100

    fix: normee

--- libft/libft.h
+++ libft/libft.h
@@ -31,7 +31,7 @@ void	*ft_memcpy(void *dest, const char *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-char  *ft_itoa(int n);
+char	*ft_itoa(int n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *str);
 char	*ft_strchr(const char *s, int c);
