f6424ef2d3f6bacfecd337985f52f4d6b5a658ed
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-13 17:41:02 +0100

    new: lstnew_bonus

---
+++ libft/ft_lstnew_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 17:27:56 by arocha-b          #+#    #+#             */
+/*   Updated: 2023/10/13 17:39:51 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *new_node;
+	
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->next = NULL;
+	new_node->content = content;
+	return (new_node);
+}
