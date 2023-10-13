2deb95d14973d9e0430b0dbb0498f1b1701e8c5e
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-13 18:11:08 +0100

    lstlast prototype

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/13 18:00:33 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/13 18:05:35 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,8 +62,9 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
-int					ft_lstsize(t_list *lst);
 
 #endif
