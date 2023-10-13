5f242252e30ed4810ff2c9257b7b8f8b5b5a175f
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-13 22:17:55 +0100

    fix: normee

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -6,17 +6,17 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 21:49:05 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/13 22:10:19 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/13 22:17:10 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list *tmp_node;
-	t_list *current_node;
+	t_list	*tmp_node;
+	t_list	*current_node;
 
 	current_node = *lst;
 	while (current_node)
