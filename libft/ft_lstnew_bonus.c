6044a360002b77237571c72ea772bccbb442775a
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-14 00:34:17 +0100

    fix: content null

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 17:27:56 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/14 00:21:26 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/14 00:33:57 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new_node;
 
 	new_node = (t_list *)malloc(sizeof(t_list));
-	
 	if (!new_node)
 		return (NULL);
 	if (!content)
@@ -26,6 +25,5 @@ t_list	*ft_lstnew(void *content)
 	else
 		new_node->content = content;
 	new_node->next = NULL;
-	
 	return (new_node);
 }
