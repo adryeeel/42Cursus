676deb2fd49bd74017e8f1181bb3e00812252546
Adryel Rocha Bezerra <arocha-b@c1r4s8.42porto.com>
2023-10-13 12:21:02 +0100

    new: ft_strmapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 12:09:14 by arocha-b          #+#    #+#             */
+/*   Updated: 2023/10/13 12:19:52 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char	*newstr;
+	size_t	index;
+	size_t	s_len;
+
+	index = 0;
+	s_len = ft_strlen(s);
+	newstr = (char *)malloc((s_len + NULL_CHAR) * sizeof(char));
+	if (!newstr)
+		return (NULL);
+	while (index < s_len)
+	{
+		newstr[index] = f(index, s[index]);
+		index++;
+	}
+	newstr[index] = '\0';
+	return (newstr);
+}
