2bd325e91e65024e7cd28d7e03561541fa460dd0
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-13 17:59:01 +0100

    lstadd_front prototype

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/13 17:40:19 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/13 17:57:32 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,53 @@
 # include <stdlib.h>
 # define NULL_CHAR 1
 
-typedef struct		s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
 
-int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-int		ft_isdigit(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
+int					ft_isalpha(int c);
+int					ft_isalnum(int c);
+int					ft_isdigit(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_tolower(int c);
+int					ft_toupper(int c);
 
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memchr(const void *str, int c, size_t n);
-void	*ft_memcpy(void *dest, const char *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void				*ft_memset(void *s, int c, size_t n);
+void				*ft_memchr(const void *str, int c, size_t n);
+void				*ft_memcpy(void *dest, const char *src, size_t n);
+void				*ft_memmove(void *dest, const void *src, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
 
-char	*ft_itoa(int n);
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *str);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-char	**ft_split(const char *s, char c);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strtrim(const char *s1, const char *set);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
+char				*ft_itoa(int n);
+size_t				ft_strlen(const char *s);
+char				*ft_strdup(const char *str);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
+char				**ft_split(const char *s, char c);
+char				*ft_strjoin(const char *s1, const char *s2);
+char				*ft_strtrim(const char *s1, const char *set);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+void				ft_striteri(char *s, void (*f)(unsigned int, char *));
+char				*ft_strmapi(const char *s, char (*f)(unsigned int, char));
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_strnstr(const char *big, const char *little,
+						size_t len);
 
-int		ft_atoi(const char *nptr);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t nmemb, size_t size);
+int					ft_atoi(const char *nptr);
+void				ft_bzero(void *s, size_t n);
+void				*ft_calloc(size_t nmemb, size_t size);
 
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
 
-t_list	*ft_lstnew(void *content);
+t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
