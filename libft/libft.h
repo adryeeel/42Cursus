d99334c84afc8632b17a4d4c1bddb151190ed6c7
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-13 17:41:18 +0100

    lstnew_bonus prototype

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:56:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/13 17:12:33 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/13 17:40:19 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,7 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+
+t_list	*ft_lstnew(void *content);
 
 #endif
