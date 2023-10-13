0b8bf0111fec126322d41e7643a6b4ddc265e9ef
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-13 17:58:28 +0100

    fix: normee

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 17:27:56 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/10/13 17:39:51 by arocha-b         ###   ########.fr       */
+/*   Updated: 2023/10/13 17:58:10 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list *new_node;
-	
+	t_list	*new_node;
+
 	new_node = (t_list *)malloc(sizeof(t_list));
 	if (!new_node)
 		return (NULL);
