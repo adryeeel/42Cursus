b6f793e08597f40b7b1fa780b127b6fb9fe31ada
Adryel Bezerra <adryelrbrz@gmail.com>
2023-10-14 16:30:47 +0100

    fix: function prototype

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -13,7 +13,7 @@
 #include "libft.h"
 #include <stdlib.h>
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	del(lst->content);
 	free(lst);
