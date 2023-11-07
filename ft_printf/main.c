10dac75d7ecbb9fdcab0b426a57e147c43cecd72
Adryel Bezerra <adryelrbrz@gmail.com>
2023-11-07 23:19:12 +0000

    mem lib added

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,30 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 10:57:47 by arocha-b          #+#    #+#             */
-/*   Updated: 2023/11/06 11:18:03 by arocha-b         ###   ########.fr       */
+/*   Created: 2023/11/07 19:43:58 by arocha-b          #+#    #+#             */
+/*   Updated: 2023/11/07 19:44:29 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "libprintf.h"
-
-int ft_printf(const char *format, ...)
-{
-	va_list arguments;
-	
-	va_start(arguments, format);
-	va_end(arguments);
-}
+#include "libs/printf/libprintf.h"
 
 int main(void)
 {
-	ft_printf(3, 1, 2, 3);
+	ft_printf();
+	return 0;
 }
+
